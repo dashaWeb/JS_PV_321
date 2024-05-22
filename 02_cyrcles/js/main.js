c9644e80bef24df0ca660d6a7384742d960ef364
konopelko_d <konopelko_d@itstep.academy>
2024-05-22 20:23:55 +0300

    cycles

---
+++ 02_cyrcles/js/main.js
@@ -0,0 +1,49 @@
+
+
+// 1 ........ 10
+// let i = 1;
+// while(i <= 10)
+// {
+//     //document.write("<p style = 'text-align:center; font-size:2em;'> " + i + " </p>")
+//     document.write(`<p style="text-align:center; font-size:2em;"> ${i} </p>`)
+//     i++;
+// }
+
+// let res;
+// do {
+//      res = prompt("2 + 2 = ?");
+// } while (res != 4);
+// alert("Good!");
+
+
+// for (let i = 2; i <=20; i+=2) {
+//     document.write(`<p style="text-align:center; font-size:2em;"> ${i} </p>`)
+// }
+// let i = 2;
+// for ( ; ;) {
+//     document.write(`<p style="text-align:center; font-size:2em;"> ${i} </p>`)
+//     i+=2
+// }
+
+
+
+
+//console.log(parseFloat("fjdkfjkj"))
+
+// document.write("<ul>")
+// while(true)
+// {
+//     let number = prompt("Enter number");
+
+//     if(number == null)
+//     {
+//         break;
+//     }
+
+//     if(isNaN(parseFloat(number)))
+//     {
+//         continue;
+//     }
+//     document.write(`<li> ${number} </li>`)
+// }
+// document.write("</ul>")
---
+++ 03_function/js/main.js
@@ -0,0 +1,50 @@
+
+
+
+// function sum(a,b = 5)
+// {
+//     return a + b;
+// }
+// console.log(sum(3))
+
+
+// let sum = function(a,b = 8)
+// {
+//     return a + b;
+// }
+// console.log(sum(3))
+
+
+// let sum = (a,b=2) => {
+//     let res = a + b;
+//     return res;
+// }
+
+// let sum = (a,b) => a + b;
+
+// console.log(sum(2,5))
+
+// let sum = (... arr) => {
+//     let s = 0;
+//     for (let i = 0; i < arr.length; i++) {
+//         s += arr[i];
+//     }
+//     return s;
+// }
+// console.log(sum(1,2,3,4,5,6,7,8,9))
+
+
+// 5! = 5 * 4!
+// 4! = 4 * 3!
+// 3! = 3 * 2!
+// 2! = 2 * 1!
+// 1! = 1
+// 0! = 1
+
+// function factorial(number)
+// {
+//     if(number == 1 || number == 0) return 1;
+//     return number * factorial(number - 1)
+// }
+
+// console.log(factorial(5))
